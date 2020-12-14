96e5e7701003ce5290493d7dbb25b5af48c73d7f
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2020-12-14 19:28:15 +0200

    Add time measurement and plot skeleton

---
+++ performance_analysis/plotting.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plot_path = 'plots/'
+
+def plot_scalability():
+    return
+
+def plot_efficiency():
+    return
+
+def plot_times():
+    return
+
+def main():
+    plot_times()
+    plot_scalability()
+    plot_efficiency()
+    return
+
+if __name__ == "__main__":
+    main()
